1c98b01b1f46c351fd1cdeb02f4f26e1fb22c36a
Cysa8010 <hishoslash@gmail.com>
2019-06-27 00:32:57 +0900

    d3dx9のlibをmain.hに追加

--- main.h
+++ main.h
@@ -11,6 +11,13 @@
 //#pragma comment(lib,"C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86\\d3d9.lib")
 //#pragma comment(lib,"C:\\Program Files (x86)\\Microsoft DirectX SDK (June 2010)\\Lib\\x86\\dxguid.lib")
 
+#include <d3dx9.h>
+#if defined(DEBUG) || defined(_DEBUG)
+#pragma comment( lib, "d3dx9d.lib" )
+#else
+#pragma comment( lib, "d3dx9.lib" )
+#endif
+
 #define SCREEN_WIDTH (1024)
 #define SCREEN_HEIGHT (576)
 
