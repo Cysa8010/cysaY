1bef7b63c9b76921d35f49b6875a5050728eb874
Cysa8010 <hishoslash@gmail.com>
2019-06-28 05:33:20 +0900

    cppとhを生成

---
+++ effect.h
@@ -0,0 +1,12 @@
+#ifndef EFFECT_H_
+#define EFFECT_H_
+
+class Effect
+{
+private:
+
+public:
+
+};
+
+#endif // !EFFECT_H_
---
+++ explosion.h
@@ -0,0 +1,12 @@
+#ifndef EXPLOSION_H_
+#define EXPLOSION_H_
+
+class Explosion
+{
+private:
+
+public:
+
+};
+
+#endif // !EXPLOSION_H_
