158419194ba28454f7bb741fa50b0bdd5e836601
Cysa8010 <hishoslash@gmail.com>
2019-06-28 03:52:18 +0900

    フレーム(骨組み)の用意

--- scene.h
+++ scene.h
@@ -1,11 +1,47 @@
 #ifndef SCENE_H_
 #define SCENE_H_
 
+// インクルードファイル
+#include <list>
+#include "Object.h"
+#include "player.h"
+#include "enemy.h"
+
 class Scene
 {
 private:
+//protected://←参考programが何故かこれになってる(シーンの拡張性?)
+	std::list<Object*> m_GameObject;
 
 public:
+	Scene(){}
+	virtual ~Scene() {}
+
+	virtual void Initialize(){}
+	virtual void Finalize(){}
+	virtual void Update(){}
+	virtual void Draw(){}
+
+	void DestroyGameObject(Object* gameObject)
+	{
+		gameObject->SetDestroy();
+	}
+
+	template <typename T> T* AddGameObject(int Layer)
+	{
+		T* object = new T();
+		object->Initialize();
+		m_GameObject[Layer].push_back(object);
+
+		return object;
+	}
+
+	template <typename T> T* GetGameObject(int Layer)
+	{
+		return (T*)nullptr;
+	}
+
+	
 
 };
 
