37d90f46cde5e6e79a7abdbac6be4bbbe962924c
Cysa8010 <hishoslash@gmail.com>
2019-06-28 03:25:38 +0900

    エネミークラスの実装

--- enemy.cpp
+++ enemy.cpp
@@ -15,10 +15,26 @@ Enemy::~Enemy()
 
 void Enemy::Initialize()
 {
+	m_Polygon = new CPolygon();
+	m_Polygon->Initialize();
+
+	m_Tex = new Texture();
+	m_Tex->Load("asset/cursol2.png");
+
 }
 
 void Enemy::Finalize()
 {
+	if (m_Tex)
+	{
+		m_Tex->Release();
+		delete m_Tex;
+	}
+	if (m_Polygon)
+	{
+		m_Polygon->Finalise();
+		delete m_Polygon;
+	}
 }
 
 void Enemy::Update()
@@ -27,4 +43,6 @@ void Enemy::Update()
 
 void Enemy::Draw()
 {
+	Renderer::GetDevice()->SetTexture(0, m_Tex->GetTexture());
+	m_Polygon->Draw(m_Position.x, m_Position.y);
 }
