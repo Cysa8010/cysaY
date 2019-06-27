86c82c58cb201bbbb60a991dce2759b3901ee16d
Cysa8010 <hishoslash@gmail.com>
2019-06-27 11:35:13 +0900

    テクスチャクラスの作成

---
+++ texture.cpp
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "renderer.h"
+#include "texture.h"
+
+void Texture::Load(const char * fileName)
+{
+	if (FAILED(D3DXCreateTextureFromFile(Renderer::GetDevice() , fileName, &image))) {
+		return;
+	}
+}
+
+void Texture::Release()
+{
+	if (image)
+	{
+		image->Release();
+		image = NULL;
+	}
+}
